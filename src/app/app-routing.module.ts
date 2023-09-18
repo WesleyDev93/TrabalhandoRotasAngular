8fb29a682cddcfd9b050cd0fc9d08c0ed1d82636
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-18 16:51:02 -0300

    Introduzindo rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Cria nossa rota passando pra ela qual o caminho , o componente e rota
+  //full quer dizer rota exata sem variação na URL
+  // prefix tem variaçãoes
+  {path: '', component: TitleComponent, pathMatch: 'full'},
+  {path: 'portifolio', component: CardComponent, pathMatch: 'prefix'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
